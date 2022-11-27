e7c711a8a291e5f6e49224ea1a657e566d7f1ef6
Tiago S. Fraga <thyaggus@gmail.com>
2022-11-27 19:46:58 -0300

    add pages support

--- src/br/com/quasar/pdf/service/PdfService.java
+++ src/br/com/quasar/pdf/service/PdfService.java
@@ -18,7 +18,7 @@ import java.util.List;
 // https://www.tutorialspoint.com/pdfbox/pdfbox_adding_pages.htm
 public class PdfService {
 
-    private static long LINHAS = 5000000;
+    private static long LINHAS = 10000;
 
     private static List<Linha> gerarCarga() {
 
@@ -49,13 +49,16 @@ public class PdfService {
     }
 
     private static void createPdf2() throws IOException {
+        String nomeArquivos = "Extrato" + LINHAS + ".pdf";
         PDDocument document = new PDDocument();
+
+        List<Linha>linhaLinkada = gerarCarga();
 
         float POINTS_PER_INCH = 72;
         float POINTS_PER_MM = 1 / (10 * 2.54f) * POINTS_PER_INCH;
 
         long linePosition = 1;
-        for (int paginas = 0; paginas < LINHAS/60; paginas ++) {
+        for (int paginas = 0; paginas < (LINHAS/60) + 1; paginas ++) {
             PDPage page = new PDPage(new PDRectangle(297 * POINTS_PER_MM, 210 * POINTS_PER_MM));
             document.addPage( page );
             PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, true);
@@ -64,20 +67,22 @@ public class PdfService {
             contentStream.setLeading(8.5f);
             contentStream.newLineAtOffset(0, 550);
 
-            for (int li = 60 * paginas; li < 60*(paginas+1); li ++ ) {
+            for (int li = 60 * paginas; li < 60*(paginas+1) && li < linhaLinkada.size(); li ++ ) {
                 if (linePosition % 2 == 0) {
                     contentStream.setFont(PDType1Font.COURIER_BOLD, 10);
                 } else {
                     contentStream.setFont(PDType1Font.COURIER, 10);
                 }
-                contentStream.showText(String.format("[%7d] ", linePosition++) + gerarCarga().get(li).getLinha());
+                contentStream.showText(String.format("[%7d] ", linePosition++) + linhaLinkada.get(li).getLinha());
                 contentStream.newLine();
 
             }
             contentStream.endText();
             contentStream.close();
+            contentStream.close();
         }
-        document.save("Extrato" + LINHAS + ".pdf");
+
+        document.save(nomeArquivos);
         document.close();
     }
 
